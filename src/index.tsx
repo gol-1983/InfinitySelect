b2898fea72a019c3fc0d2abc2c986d83bc8731e2
gol-1983 <99811910+gol-1983@users.noreply.github.com>
2023-06-25 16:34:33 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-    <HashRouter basename="/infinitySelect">
+    
     <Provider store={store}>
       <App />
     </Provider>
-    </HashRouter>
+    
 );
 
 reportWebVitals();
